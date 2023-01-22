cee5b76af43289d6d28f8e86df95d37844a3c437
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-01-22 15:46:58 -0600

    added a form submit handler

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 import { NextPage } from "next";
 
 interface Props {}
-interface FormContainerProps {
-  children?: JSX.Element[] | JSX.Element;
-}
+
 interface TextInputProps {
   labelName: string;
   placeholder: string;
@@ -46,7 +44,7 @@ const TextInput: React.FC<TextInputProps> = ({
         className={
           error
             ? `bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500`
-            : `bg-green-50 border border-gray-500 text-green-900 dark:text-gray-400 placeholder-gray-700 dark:placeholder-gray-500 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-500`
+            : `bg-green-50 border border-gray-500 text-gray-900 dark:text-gray-400 placeholder-gray-700 dark:placeholder-gray-500 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-500`
         }
         value={value}
         placeholder={placeholder}
@@ -64,22 +62,81 @@ const TextInput: React.FC<TextInputProps> = ({
   );
 };
 
-const FormContainer: React.FC<FormContainerProps> = ({ children }) => {
+const InfoContainer: React.FC = () => {
   return (
-    <div className="w-1/2 bg-slate-300 px-8 py-8 rounded-xl">
-      <h1 className="text-center text-2xl text-gray-700 dark:text-gray-500 font-bold">
-        Contact Us
-      </h1>
-      <form>{children}</form>
+    <div className="text-gray-900 dark:text-gray-400 w-1/3 p-8 flex flex-col justify-evenly">
+      <div className="rounded-lg border-solid border-2 bg-slate-500 text-white border-gray-500 p-2 shadow-md">
+        <h2>Phone:</h2>
+        <p className="">999-999-9999</p>
+      </div>
+
+      <div className="rounded-lg border-solid border-2 bg-slate-500 text-white border-gray-500 p-2 shadow-md">
+        <h2>Address:</h2>
+        <p>123 fake St, 99999</p>
+        <p className="">Fake city, Texas</p>
+      </div>
+      <div className="rounded-lg border-solid border-2 bg-slate-500 text-white border-gray-500 p-2 shadow-md">
+        <h2>Hours:</h2>
+        <table className="border-spacing-4">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td className="pr-2">Mon</td>
+              <td>8:00am</td>
+              <td>-</td>
+              <td>6:00pm</td>
+            </tr>
+            <tr>
+              <td className="pr-2">Tue</td>
+              <td>8:00am</td>
+              <td>-</td>
+              <td>6:00pm</td>
+            </tr>
+            <tr>
+              <td className="pr-2">Wed</td>
+              <td>8:00am</td>
+              <td>-</td>
+              <td>6:00pm</td>
+            </tr>
+            <tr>
+              <td className="pr-2">Thr</td>
+              <td>8:00am</td>
+              <td>-</td>
+              <td>6:00pm</td>
+            </tr>
+            <tr>
+              <td className="pr-2">Fri</td>
+              <td>8:00am</td>
+              <td>-</td>
+              <td>6:00pm</td>
+            </tr>
+            <tr>
+              <td className="pr-2">Sat</td>
+              <td>8:00am</td>
+              <td>-</td>
+              <td>3:00pm</td>
+            </tr>
+            <tr>
+              <td className="pr-2">Sun</td>
+              <td>8:00am</td>
+              <td>-</td>
+              <td>3:00pm</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
 
-const contact: NextPage<Props> = () => {
+const ContactContainer: React.FC = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [phone, setPhone] = useState("");
+
+  const [formErr, setFormErr] = useState(false);
+  const [formSuccess, setformSuccess] = useState(false);
 
   const [nameErr, setNameErr] = useState(false);
   const [emailErr, setEmailErr] = useState(false);
@@ -168,54 +225,146 @@ const contact: NextPage<Props> = () => {
     }
   };
 
+  const resetAll = () => {
+    setName("");
+    setEmail("");
+    setMessage("");
+    setPhone("");
+    setNameErr(false);
+    setEmailErr(false);
+    setMessageErr(false);
+    setPhoneErr(false);
+    setFormErr(false);
+  };
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    setNameErr(false);
+    setEmailErr(false);
+    setMessageErr(false);
+    setPhoneErr(false);
+    setFormErr(false);
+
+    if (!name) {
+      setNameErr(true);
+      return;
+    }
+    if (!email) {
+      setEmailErr(true);
+      return;
+    }
+    if (!phone) {
+      setPhoneErr(true);
+      return;
+    }
+    if (!message) {
+      setMessageErr(true);
+      return;
+    }
+    if (!validPhone.test(phone)) {
+      setPhoneErr(true);
+      return;
+    }
+    if (!validEmail.test(email)) {
+      setEmailErr(true);
+      return;
+    }
+    setformSuccess(true);
+    resetAll();
+  };
+
+  return (
+    <div className="bg-slate-300 rounded-xl w-1/2 flex relative">
+      <InfoContainer />
+
+      <div className="px-8 py-8 grow">
+        <h1 className="text-center text-2xl text-gray-700 dark:text-gray-500 font-bold">
+          Contact Us
+        </h1>
+
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            labelName="Your Name"
+            placeholder="enter your name"
+            error={nameErr}
+            errorMessage="Please enter your name"
+            value={name}
+            handler={handleName}
+            htmlAttr={"name"}
+            focusHandler={handleNameFocus}
+            blurHandler={handleNameBlur}
+          />
+          <TextInput
+            labelName="Your Email"
+            placeholder="enter your email"
+            error={emailErr}
+            errorMessage="Incorrect Email Format"
+            value={email}
+            handler={handleEmail}
+            htmlAttr={"email"}
+            focusHandler={handleEmailFocus}
+            blurHandler={handleEmailBlur}
+          />
+          <TextInput
+            labelName="Your Phone"
+            placeholder="enter your phone number"
+            error={phoneErr}
+            errorMessage="Incorrect Phone Number Format"
+            value={phone}
+            handler={handlePhone}
+            htmlAttr={"phone"}
+            focusHandler={handlePhoneFocus}
+            blurHandler={handlePhoneBlur}
+          />
+          <TextInput
+            labelName="Your Message"
+            placeholder="enter your message"
+            error={messageErr}
+            errorMessage="Please enter your message"
+            value={message}
+            handler={handleMessage}
+            htmlAttr={"message"}
+            focusHandler={handleMessageFocus}
+            blurHandler={handleMessageBlur}
+          />
+          <div className="px-4 py-3 text-right sm:px-6">
+            <button
+              type="reset"
+              onClick={resetAll}
+              className="inline-flex justify-center w-1/6 py-2 px-4 mr-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-slate-800 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-300"
+            >
+              Reset
+            </button>
+
+            <button
+              type="submit"
+              className="inline-flex justify-center w-1/4 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-slate-500 hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-300"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+
+      {formSuccess && (
+        <div className="bg-green-200 px-10 py-4 rounded-lg absolute bottom-0 translate-y-[100%] -translate-x-[50%] left-[50%]">
+          <span>Your Message has Successfully been sent!</span>
+          <span
+            className="ml-2 text-gray-500 cursor-pointer"
+            onClick={() => setformSuccess(false)}
+          >
+            x
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const contact: NextPage<Props> = () => {
   return (
     <div className="bg-slate-600 h-[100vh] grid place-items-center">
-      <FormContainer>
-        <TextInput
-          labelName="Your Name"
-          placeholder="enter your name"
-          error={nameErr}
-          errorMessage="Please enter your name"
-          value={name}
-          handler={handleName}
-          htmlAttr={"name"}
-          focusHandler={handleNameFocus}
-          blurHandler={handleNameBlur}
-        />
-        <TextInput
-          labelName="Your Email"
-          placeholder="enter your email"
-          error={emailErr}
-          errorMessage="Incorrect Email Format"
-          value={email}
-          handler={handleEmail}
-          htmlAttr={"email"}
-          focusHandler={handleEmailFocus}
-          blurHandler={handleEmailBlur}
-        />
-        <TextInput
-          labelName="Your Phone"
-          placeholder="enter your phone number"
-          error={phoneErr}
-          errorMessage="Incorrect Phone Number Format"
-          value={phone}
-          handler={handlePhone}
-          htmlAttr={"phone"}
-          focusHandler={handlePhoneFocus}
-          blurHandler={handlePhoneBlur}
-        />
-        <TextInput
-          labelName="Your Message"
-          placeholder="enter your message"
-          error={messageErr}
-          errorMessage="Please enter your message"
-          value={message}
-          handler={handleMessage}
-          htmlAttr={"message"}
-          focusHandler={handleMessageFocus}
-          blurHandler={handleMessageBlur}
-        />
-      </FormContainer>
+      <ContactContainer />
     </div>
   );
 };
