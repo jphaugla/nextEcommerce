640f8c75dcdfbf43d4c7f750f9a839cc54448fc0
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-01-24 18:31:26 -0600

    used nextauth event to populate cartId in User table with prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,23 @@
 import NextAuth from "next-auth";
+import { v4 as uuidv4 } from 'uuid';
 // import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 // import clientPromise from "../../../lib/mongodb";
 
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+
+import type { AuthOptions } from "next-auth";
 const prisma = new PrismaClient()
 
+
+interface Message {
+  id: string;
+  name: string;
+  email: string;
+  emailVerified: null | boolean;
+  image: string;
+  cartId: null | string
+}
 
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
@@ -14,22 +26,32 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        }
-      },
+      // profile(profile) {
+      //   return {
+      //     id: profile.sub,
+      //     name: profile.name,
+      //     email: profile.email,
+      //     image: profile.picture,
+      //   }
+      // },
     }),
   ],
 
   secret: process.env.JWT_SECRET,
   adapter: PrismaAdapter(prisma),
+  events: {
+    createUser: async (message: any) => {
+      const updateUser = await prisma.user.update({
+        where: {
+          id: message.user.id
+        },
+        data: {
+          cartId: uuidv4()
+        }
+      })
+    }
 
-
+  }
   // callbacks: {
   //   async jwt(token, account) {
   //     if (account?.accessToken) {
