4726f2fee3f5e3a4bec5a0351d7d28282e269079
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-02-24 17:26:07 -0600

    updated schema and planetscale database due to an uncaught table relation error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,58 +1,38 @@
 import NextAuth from "next-auth";
-import { v4 as uuidv4 } from 'uuid';
+import type { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-import type { AuthOptions } from "next-auth";
+import { v4 as uuidv4 } from 'uuid';
+
 const prisma = new PrismaClient()
 
-interface Message {
-  id: string;
-  name: string;
-  email: string;
-  emailVerified?: boolean;
-  image: string;
-  cartId?: string
-}
-
-import GoogleProvider from "next-auth/providers/google";
 export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      // profile(profile) {
-      //   return {
-      //     id: profile.sub,
-      //     name: profile.name,
-      //     email: profile.email,
-      //     image: profile.picture,
-      //   }
-      // },
     }),
   ],
-
   secret: process.env.JWT_SECRET,
   adapter: PrismaAdapter(prisma),
   events: {
     createUser: async (message) => {
-
       const generatedCartId = uuidv4()
-      const updatedUser = await prisma.user.update({
-        where: {
-          id: message.user.id
-        },
-        data: {
-          cartId: generatedCartId
-        }
-      })
+      // const updatedUser = await prisma.user.update({
+      //   where: {
+      //     id: message.user.id
+      //   },
+      //   data: {
+      //     cartId: generatedCartId
+      //   }
+      // })
       const userCart = await prisma.cart.create({
         data: {
           id: generatedCartId,
-          userId: updatedUser.id,
+          userId: message.user.id
         },
       })
-      console.log('updated User: ', updatedUser)
-      console.log('user Cart: ', userCart)
     }
   }
 };
