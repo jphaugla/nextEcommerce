c3f7b657233cbe393cb37dc3a88ab05cd8c4a69b
Steven <stevenmb1995@gmail.com>
2024-04-11 03:30:09 -0500

    added temporary logger to debug redirects

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,6 +6,45 @@ import { v4 as uuidv4 } from 'uuid';
 import { prisma } from "../../../services/prisma-client"
 
 
+//Debouncing Logging errors for callbacks to GoogleOAuth for debugging
+const token = process.env.LOGGING
+const method = 'sendMessage'
+const url = `https://api.telegram.org/bot${token}/${method}`
+let timeoutId: NodeJS.Timeout | null = null;
+
+async function handler(data:any) {
+  try {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(async () => {
+      const telRes = await fetch(url, {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data || {test:"test"})
+      });
+      const telData = await telRes.json();
+      console.log("telData", telData);
+      timeoutId = null; // Clear the timeout ID after execution
+    }, 1500); // Adjust the debounce duration as needed (e.g., 500 milliseconds)
+  } catch (error:any) {
+    console.error("Error sending message:", error);
+  }
+}
+
+async function debouncedApiCall(callbackParams: any) {
+  try {
+    const data = {
+      chat_id: process.env.TELEGRAM_ID,
+      text: JSON.stringify({ callbackParams })
+    }
+    handler(data)
+  } catch (error) {
+    console.error("Error performing API call:", error);
+  }
+}
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -24,6 +63,23 @@ export const authOptions: AuthOptions = {
   //     return params.baseUrl;
   //   },
   // },
+  callbacks:{
+    async redirect(callbackParams) {
+
+      console.log("Redirect callbackParams:", callbackParams);
+      debouncedApiCall(callbackParams)
+
+      // Handle errors related to the redirect
+      // if (callbackParams?.error) {
+      //   console.error("Redirect error:", callbackParams.error);
+      //   // You can choose to redirect the user to an error page or handle it accordingly
+      //   throw new Error("Redirect error occurred");
+      // }
+
+      // Return the base URL
+      return callbackParams.baseUrl;
+    },
+  },
   events: {
     createUser: async (message) => {
       const generatedCartId = uuidv4()
