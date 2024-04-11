96c71f15578a2cd983be433f32acad83771cb415
Steven <stevenmb1995@gmail.com>
2024-04-11 01:08:04 -0500

    changed uri for backend on apollo client

--- services/apollo-client.ts
+++ services/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   ssrMode: true,
-  uri: "https://misty-night-9009.fly.dev/graphql",
+  uri: "https://graphqlecommerce.fly.dev/graphql",
   cache: new InMemoryCache(),
 });
 
